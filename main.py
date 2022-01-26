cd57ee01e9cd9c21ae4730cb5bb8497efb452071
Joshua Humphrey <joshhumphrey1@gmail.com>
2022-01-26 12:18:35 -0600

    Can add a new combatant to the turn order.

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import time
 import random
 import tkinter as tk
+from tkinter import ttk
 from tkinter.messagebox import showinfo
 from characters import characters
 
 
+class Tracker:
+  def __init__(self):
+    self.characters = characters
+    self.new_mod = None
+
+def get_new_mod(selection):
+  global new_mod
+  new_mod = selection
+ 
+  
+
+  
+def add_new_combatant(name,mod):
+  new_combatant = {
+    id: len(characters),
+    "name": name,
+    "modifier": mod,
+    "result": 0,
+    "active": True
+  }
+  characters.append(new_combatant)
+  create_character_frames(characters)
 def toggle_active(i):
 
   characters[i]['active'] = not characters[i]['active']
   characters[i]['result'] = 0
-  
-  print(characters[i])
   create_character_frames(characters)
 
 def delete_character(i,char_frame):
@@ -18,10 +39,12 @@ def delete_character(i,char_frame):
   del characters[i]
   char_frame.destroy()
   create_character_frames(characters)
-buttons = []
-delete_buttons = []
+
 def create_character_frames(characters):
+  buttons = []
+  delete_buttons = []
   for i in range(len(characters)):
+    
     current_character = characters[i]
     frame_name = 'frame_' + current_character['name']
     char_frame = tk.Frame(left_column, name=frame_name)
@@ -43,7 +66,7 @@ def create_character_frames(characters):
     buttons[i].grid(row=1, column=3)
     delete_buttons[i].grid(row=1, column=4)
     char_frame.grid(row=i+1, column=0)
-    
+  
 def update_character_frames(characters):
   for i in range(len(characters)):
     current_character = characters[i]
@@ -81,9 +104,10 @@ root.geometry('500x500')
 root.title('Trackerbot v1')
 
 left_column = tk.Frame(root)
-left_column.grid(row=0, column=1,sticky=tk.NW)
+left_column.grid(row=0, column=0,sticky=tk.NW)
 turn_order = tk.Frame(root)
-turn_order.grid(row=0, column=2,sticky=tk.NE)
+
+turn_order.grid(row=0, column=3,sticky=tk.NE)
 tk.Label(turn_order, text="Turn Order").grid(row=0, column=0)
 
 
@@ -95,16 +119,31 @@ create_character_frames(characters)
 
 
 
-mods = (1,2,3,4,5,6,7,8,9,10,11,12)
+mods = [-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10,11,12]
 
 var = tk.StringVar(value=dir(tk))
 var.set(mods)
 
-
+global new_mod
+new_mod = None
 # lb.grid(row=1, column = 0)
 
-roll_button.grid(row=len(characters)+1, column=1,sticky=tk.S)
+
+
+roll_button.grid(row=len(characters)+1, column=0,sticky=tk.S)
 # lb.bind('<<ListboxSelect>>', clickEvent)
+new_char_name = ttk.Entry(root,textvariable=tk.StringVar())
+new_char_name.grid(row=len(characters)+2, column=0,sticky=tk.S)
+placeholder_mod = tk.StringVar(root)
+placeholder_mod.set("Modifier")
+mod_select = tk.OptionMenu(root,placeholder_mod,*mods,command=get_new_mod)
+mod_select.grid(row=len(characters)+3, column=0,sticky=tk.S)
+#button
+add_new_char_button = tk.Button(root, text="Add New Character", command=lambda: add_new_combatant(new_char_name.get(), new_mod))
+add_new_char_button.grid(row=len(characters)+4, column=0,sticky=tk.S)
+#button
+
+
 
 
 
